2e8b169d353705f1bfc617d1853a5280e579bac0
Shannen <shannen.ye@gmail.com>
2018-06-28 00:24:44 -0400

    changed postgres url

--- CalendarBackend/db/db.js
+++ CalendarBackend/db/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 const db = new Sequelize(
-    process.env.DATABASE_URL || 'postgres://127.0.0.1:5432/spotify', {
+    process.env.DATABASE_URL || 'postgres://127.0.0.1:5432/calendar', {
         logging: false
     }
 );
