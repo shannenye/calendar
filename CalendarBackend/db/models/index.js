515002d19cd6a0058b9a2f3d42866cce3cb2670c
Shannen <shannen.ye@gmail.com>
2018-06-28 00:26:01 -0400

    removed incorrect code

--- CalendarBackend/db/models/index.js
+++ CalendarBackend/db/models/index.js
@@ -1,15 +1,7 @@
-// const Month = require('./month.js');
-// const Day = require('./day.js');
 const Events = require('./event.js');
 const User = require('./user.js');
 
-// User.hasMany(Events);
-// Day.hasMany(Events);
-// Month.hasMany(Day);
-
 module.exports = {
     User,
-    // Day,
-    // Month,
     Events
 };
