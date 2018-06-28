afac0e99c886694e0ebca7c9cb1a70d590f7d6ea
Shannen <shannen.ye@gmail.com>
2018-06-28 00:27:08 -0400

    updated dayView component

--- CalendarFrontEnd/browser/components/DayView.js
+++ CalendarFrontEnd/browser/components/DayView.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from  'react-redux';
+import { Link } from 'react-router-dom';
+import Popup from 'reactjs-popup';
 import { getEvents, updateEvent, deleteEvent } from '../store/events';
+import UpdateEvent from './UpdateEvent';
 
 class DayView extends Component {
+    constructor(props) {
+        super(props);
 
+    }
     componentDidMount() {
         this.props.getEvents();
-    }
-
-    handleClick(id) {
-        this.props.deleteEvent(id)
     }
 
     render() {
@@ -17,15 +19,29 @@ class DayView extends Component {
 
         return (
             <div>
+                <Link to={`/`}>
+                    <div>
+                        <button className="back">Go Back</button>
+                    </div>
+                </Link>
                 {this.props.loadEvents.map(event => {
-                    if (event.day === currentDay) {
+                    if (currentDay == event.day) {
                         return (
-                            <div className="allEvents">
-                                <li>{event.title}</li>
-                                <li>{event.start}</li>
-                                <li>{event.end}</li>
-                                <li>{event.description}</li>
-                                <button className="deleteButton" onClick={() => this.handleClick(event.id)}>Delete Event</button>
+                            <div key={currentDay} className="allEvents">
+                                <li key={event.id}>Event ID: {event.id}</li>
+                                <li key={event.title}>Event Name: {event.title}</li>
+                                <li key={event.start}>Start Time: {event.start}</li>
+                                <li key={event.end}>End Time: {event.end}</li>
+                                <li key={event.description}>Description: {event.description}</li>
+                                <button className="deleteButton" onClick={() => this.props.deleteEvent(event.id)}>Delete Event</button>
+                                <Popup trigger={<button className="event">Update Event</button>} modal>
+                                    {close => (
+                                        <div className="modal">
+                                            <a className="close" onClick={close}>Enter Updated Info</a>
+                                            <UpdateEvent id={currentDay} eventID={event.id} close={close}/>
+                                        </div>
+                                    )}
+                                </Popup>
                             </div>
                         )
                     }
